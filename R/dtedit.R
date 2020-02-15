3cb6002d009bf166adb492e0eb66299f77603d56
austin-ith <ith19003@byui.edu>
2020-02-15 09:41:08 -0700

    s

--- R/dtedit.R
+++ R/dtedit.R
@@ -393,10 +393,7 @@ dtedit <- function(input, output, name, thedata, thedata2,
 		output[[paste0(name, '_message')]] <- renderText('')
 		fields <- getFields('_edit_', values=result$thedata[row,])
 		shiny::modalDialog(title = title.edit,
-		                   HTML("<b>"),
-		                   paste("From:"),
-		                   HTML("</b>"),
-		                   ship_from,
+		                   paste(HTML("<b>"),"From: ", HTML("</b>"), ship_from),
 		                   HTML("<br>"),
 		                   paste(tags$strong("To: ")),
 		                    result$thedata[row,]$address,
